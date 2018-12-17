3ae7e591cb593e7c04438433835652446c2ab355
荆拓 <jingtuo@foundersc.com>
2018-12-17 17:52:04 +0800

    项目初始化

---
+++ app/src/main/java/com/jingtuo/android/life/MainActivity.java
@@ -0,0 +1,17 @@
+package com.jingtuo.android.life;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 主页
+ * @author JingTuo
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
